d1ade55ea311f39f61c7082ada74d6252c1f6988
Fabian Finke <finkef@icloud.com>
2026-06-06 22:18:54 +0200

    Export named io-ts-types codec interfaces from core (#41)
    * Export io-ts-types codec interfaces
    
    * Export io-ts-types codec interfaces from core
    
    * Pin TypeScript 4.5.5 with Yarn resolution

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -3,23 +3,56 @@ import * as t from "io-ts"
 import { clone } from "io-ts-types/lib/clone"
 
 // Curated set of io-ts-types helpers exposed through `t`.
-export { BigIntFromString } from "io-ts-types/lib/BigIntFromString"
-export { BooleanFromNumber } from "io-ts-types/lib/BooleanFromNumber"
-export { BooleanFromString } from "io-ts-types/lib/BooleanFromString"
-export { DateFromISOString } from "io-ts-types/lib/DateFromISOString"
-export { DateFromNumber } from "io-ts-types/lib/DateFromNumber"
-export { DateFromUnixTime } from "io-ts-types/lib/DateFromUnixTime"
-export { IntFromString } from "io-ts-types/lib/IntFromString"
+export {
+  BigIntFromString,
+  type BigIntFromStringC,
+} from "io-ts-types/lib/BigIntFromString"
+export {
+  BooleanFromNumber,
+  type BooleanFromNumberC,
+} from "io-ts-types/lib/BooleanFromNumber"
+export {
+  BooleanFromString,
+  type BooleanFromStringC,
+} from "io-ts-types/lib/BooleanFromString"
+export {
+  DateFromISOString,
+  type DateFromISOStringC,
+} from "io-ts-types/lib/DateFromISOString"
+export {
+  DateFromNumber,
+  type DateFromNumberC,
+} from "io-ts-types/lib/DateFromNumber"
+export {
+  DateFromUnixTime,
+  type DateFromUnixTimeC,
+} from "io-ts-types/lib/DateFromUnixTime"
+export {
+  IntFromString,
+  type IntFromStringC,
+} from "io-ts-types/lib/IntFromString"
 export {
   Json,
   JsonArray,
   JsonFromString,
   JsonRecord,
 } from "io-ts-types/lib/JsonFromString"
-export { NonEmptyString } from "io-ts-types/lib/NonEmptyString"
-export { nonEmptyArray } from "io-ts-types/lib/nonEmptyArray"
-export { NumberFromString } from "io-ts-types/lib/NumberFromString"
-export { readonlyNonEmptyArray } from "io-ts-types/lib/readonlyNonEmptyArray"
+export {
+  NonEmptyString,
+  type NonEmptyStringC,
+} from "io-ts-types/lib/NonEmptyString"
+export {
+  nonEmptyArray,
+  type NonEmptyArrayC,
+} from "io-ts-types/lib/nonEmptyArray"
+export {
+  NumberFromString,
+  type NumberFromStringC,
+} from "io-ts-types/lib/NumberFromString"
+export {
+  readonlyNonEmptyArray,
+  type ReadonlyNonEmptyArrayC,
+} from "io-ts-types/lib/readonlyNonEmptyArray"
 export { withFallback } from "io-ts-types/lib/withFallback"
 export { withMessage } from "io-ts-types/lib/withMessage"
 
